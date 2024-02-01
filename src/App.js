9fe32b7c3932933de75be51db8b92f8881a3652d
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2024-02-01 15:41:18 +1300

    Add Redux actions and reducers

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import DisplayBalance from './components/DisplayBalance';
 import DisplayBalances from './components/DisplayBalances';
 import EntryLines from './components/EntryLines';
 import ModalEdit from './components/ModalEdit';
-import { createStore, configureStore } from 'redux';
 
 import './App.css';
 
@@ -20,11 +19,6 @@ function App() {
 	const [incomeTotal, setIncomeTotal] = useState(0);
 	const [expenseTotal, setExpenseTotal] = useState(0);
 	const [total, setTotal] = useState(0);
-
-	function deleteEntry(id) {
-		const result = entries.filter((entry) => entry.id !== id);
-		setEntries(result);
-	}
 
 	function addEntry() {
 		const result = entries.concat({
@@ -50,46 +44,16 @@ function App() {
 		}
 	}
 
+	function deleteEntry(id) {
+		const result = entries.filter((entry) => entry.id !== id);
+		setEntries(result);
+	}
+
 	function resetEntry() {
 		setDescription('');
 		setValue('');
 		setIsExpense(true);
 	}
-
-	const store = createStore((state = initialEntries, action) => {
-		console.log(action);
-		let newEntries;
-		switch(action.type){
-			case 'ADD_ENTRY':
-				newEntries = state.concat({...action.payload});
-				return newEntries;
-			
-			case 'REMOVE_ENTRY':
-				newEntries = state.filter((entry) => entry.id !== action.payload.id);
-				return newEntries;
-				
-			default:
-				return state;
-		};
-	});
-
-	store.subscribe(() => {
-		console.log('Store: ', store.getState())
-	});
-
-	const payload_add = {
-		id: entries.length + 1,
-		description: 'Hello from Redux',
-		value: 100,
-		isExpense: false
-	};
-
-	const payload_remove = {
-		id: 7
-	};
-	
-	store.dispatch({ type: 'ADD_ENTRY', payload: payload_add });
-	store.dispatch({ type: 'REMOVE_ENTRY', payload: payload_remove });
 
 	useEffect(() => {
 		if(!isOpen && entryId){
